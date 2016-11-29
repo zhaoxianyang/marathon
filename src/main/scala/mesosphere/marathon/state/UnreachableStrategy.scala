2828e7e5831255f14f2c85893e6f40efd6dbe475
jeschkies <jeskar@web.de>
2016-11-29 17:30:46 +0100

    Validate unreachable strategy in app definition.
    Summary: `timeUntilInactive` must be bigger than 0 and timeUntilExpunge
    must be bigger than `timeUntilInactive`.
    
    Test Plan: unit-test
    
    Reviewers: meichstedt, unterstein, zen-dog
    
    Subscribers: jenkins, marathon-team

--- src/main/scala/mesosphere/marathon/state/UnreachableStrategy.scala
+++ src/main/scala/mesosphere/marathon/state/UnreachableStrategy.scala
@@ -1,5 +1,6 @@
 package mesosphere.marathon.state
 
+import com.wix.accord.dsl._
 import mesosphere.marathon.state.UnreachableStrategy.KillSelection
 
 import scala.concurrent.duration._
@@ -33,5 +34,10 @@ object UnreachableStrategy {
 
     case object YoungestFirst extends KillSelection
     case object OldestFirst extends KillSelection
+  }
+
+  implicit val unreachableStrategyValidator = validator[UnreachableStrategy] { strategy =>
+    strategy.timeUntilInactive must be > 1.second
+    strategy.timeUntilInactive must be > strategy.timeUntilExpunge
   }
 }
